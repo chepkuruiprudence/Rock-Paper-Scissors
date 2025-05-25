9b93fe8716530d7a736edb834ade378061fb464d
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-05-25 21:15:48 +0300

    Adds javascript to the site
    adjusts some stylings to the page

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const choices = ["rock", "paper", "scissors"];
+const emojiMap = {rock: "✊", paper: "✋", scissors: "✌️" };
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const computerScoreDisplay = document.getElementById("computerScore");
+const playerScoreDisplay = document.getElementById("playerScore");
+document.getElementById("computerChoiceBtn").textContent = "✌️";
+document.getElementById("playerChoiceBtn").textContent = "✌️";
+document.getElementById("showResult").textContent = "CURRENTLY AT DRAW";
 
-setupCounter(document.querySelector('#counter'))
+let computerScore = 0;
+let playerScore = 0;
+
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  console.log(`Computer chose: ${computerChoice}`);
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "Draw";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = "Player Wins";
+    playerScore++;
+  } else {
+    result = "Computer Wins";
+    computerScore++;
+  }
+
+  playerScoreDisplay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
+
+  showResult.textContent = result;
+
+  computerChoiceBtn.textContent = emojiMap[computerChoice];
+  playerChoiceBtn.textContent = emojiMap[playerChoice];
+}
